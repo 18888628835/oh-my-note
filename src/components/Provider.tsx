8e92ffc8a1ebf2ff073d7a42bdef88bb23840be9
qiuyanxi <qiuyanxi1992@gmail.com>
2023-07-16 23:00:21 +0800

    fix: remove injectStores

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,12 +1,8 @@
 'use client'
-import { injectStores } from '@mobx-devtools/tools'
 import { SessionProvider } from 'next-auth/react'
 import { PropsWithChildren, FC } from 'react'
 import { menuItems, MenuContext } from 'src/store/MenuItems'
 
-injectStores({
-  menuItems,
-})
 const Provider: FC<PropsWithChildren> = ({ children }) => {
   return (
     <SessionProvider>
