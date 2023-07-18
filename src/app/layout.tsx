3fe7005914e19b2b16c1ab0f9f54ad2a82733425
qiuyanxi <qiuyanxi1992@gmail.com>
2023-07-18 18:44:04 +0800

    feat: search via algolia

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import 'src/style/markdown.css'
 import 'src/style/variable.css'
 import 'src/style/reset.css'
 import 'src/style/global.css'
+import '@docsearch/css'
 import { Metadata } from 'next'
 import Container from 'src/app/docs/Container'
 import BackTop from 'src/components/BackTop'
@@ -14,12 +15,25 @@ import StyledJsxRegistry from 'src/lib/registry'
 export const metadata: Metadata = {
   title: AppConfig.brand,
   description: '业余时间手写的文档系统，记录着我的心得笔记。',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    minimumScale: 1,
+    userScalable: false,
+    viewportFit: 'cover',
+  },
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const nav = await getNavigation()
   return (
     <html lang="en">
+      <link
+        rel="preconnect"
+        href={`https://${process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}-dsn.algolia.net`}
+        crossOrigin="anonymous"
+      />
       <body>
         <StyledJsxRegistry>
           <Provider>
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,7 +1,13 @@
 import { DocSearch } from '@docsearch/react'
-import '@docsearch/css'
+
 const Search = () => {
-  return <DocSearch appId="R2IYF7ETH7" apiKey="599cec31baffa4868cae4e79f180729b" indexName="docsearch" />
+  return (
+    <DocSearch
+      appId={process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ?? ''}
+      apiKey={process.env.NEXT_PUBLIC_ALGOLIA_API_KEY ?? ''}
+      indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME ?? ''}
+    />
+  )
 }
 
 export default Search
